61b6abdd2bb4f40faafcd1b953bbdc02df14746a
José Cardoso <zehcardoso@outlook.com>
2020-05-11 22:35:46 -0300

    Mostrando o Usuário Logado

---
+++ src/main/java/com/televisivo/web/ResourcesExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.televisivo.web;
+
+import com.televisivo.security.UsuarioSistema;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class ResourcesExceptionHandler {
+    
+    @ModelAttribute("usuario_logado")
+    public UsuarioSistema getUsuarioLogado(@AuthenticationPrincipal UsuarioSistema usuario_logado) {
+        return (usuario_logado == null) ? null : usuario_logado;
+    }
+}
