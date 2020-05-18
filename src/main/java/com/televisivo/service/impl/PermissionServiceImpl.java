c9f0678177a33aec5561630bdfe0bd9e989cb685
José Cardoso <zehcardoso@outlook.com>
2020-05-18 18:15:20 -0300

    Verificação dos direitos de acesso ao método

--- src/main/java/com/televisivo/service/impl/PermissionServiceImpl.java
+++ src/main/java/com/televisivo/service/impl/PermissionServiceImpl.java
@@ -1,6 +1,9 @@
 package com.televisivo.service.impl;
 
+import com.televisivo.model.Role;
+import com.televisivo.model.RolePermissao;
 import com.televisivo.model.Usuario;
+import com.televisivo.security.UsuarioSistema;
 import com.televisivo.service.PermissionService;
 
 import org.springframework.security.core.Authentication;
@@ -9,7 +12,26 @@ public class PermissionServiceImpl implements PermissionService {
 
     @Override
     public boolean hasPermission(Authentication usuarioLogado, Object permissao, Object escopo) {
-        return false;
+        boolean toReturn = false;
+        boolean true_permissao = false;
+        boolean true_escopo = false;
+
+        UsuarioSistema usuarioSistema = (UsuarioSistema) usuarioLogado.getPrincipal();
+        Usuario usuario = this.findRolePermissaoByUsuarioId(usuarioSistema.getUsuario().getId());
+        for (Role role : usuario.getRoles()) {
+            for (RolePermissao rolePermissao : role.getRolePermissoes()) {
+                true_permissao = permissao.equals(rolePermissao.getPermissao().getNome().toUpperCase()) ? true : false;
+                true_escopo = escopo.equals(rolePermissao.getEscopo().getNome().toUpperCase()) ? true : false;
+                if (true_permissao && true_escopo) {
+                    toReturn = true;
+                    break;
+                }
+            }
+            if (toReturn == true) {
+                break;
+            }
+        }
+        return toReturn;
     }
 
     @Override
