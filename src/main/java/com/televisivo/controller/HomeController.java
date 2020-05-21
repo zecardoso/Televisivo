059583c9239173fb8758a0c4516bd7aa3974be0f
José Cardoso <zehcardoso@outlook.com>
2020-05-21 01:04:09 -0300

    Aplicando Segurança na URL /home

--- src/main/java/com/televisivo/controller/HomeController.java
+++ src/main/java/com/televisivo/controller/HomeController.java
@@ -1,17 +1,26 @@
 package com.televisivo.controller;
 
+import com.televisivo.security.UsuarioSistema;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping(value = "/home")
-    public String home() {
+    public String home(@AuthenticationPrincipal UsuarioSistema usuario_logado, ModelMap model) {
+        if (usuario_logado == null) {
+            return "/login";
+        }
+        model.addAttribute("usuarioLogado", usuario_logado);
         return "home";
     }
 
