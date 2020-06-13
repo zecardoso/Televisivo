c09e0baf8353402184aa14470d61361f873cf67b
José Cardoso <zehcardoso@outlook.com>
2020-06-13 04:18:58 -0300

    Alterando mapeamento de login

--- src/main/java/com/televisivo/web/controller/HomeController.java
+++ src/main/java/com/televisivo/web/controller/HomeController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -25,7 +27,7 @@ public class HomeController {
         return "home";
     }
 
-    @GetMapping(value = "/login")
+    @RequestMapping(value = { "/", "/login" }, method = { RequestMethod.GET, RequestMethod.POST })
     public String loginPage(@RequestParam(value = "mensagem", required = false) String mensagem, Model model) {
         if (Objects.isNull(mensagem)) {
             model.addAttribute("acao", false);
